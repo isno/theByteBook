ebb3de18327f4d9fb748463c0aef14f189fc7de7
isno <isno.cn@gmail.com>
2023-03-12 13:11:41 +0800

    更改为vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,25 @@
+import { defineUserConfig, defaultTheme } from 'vuepress';
+
+export default defineUserConfig({
+    lang: 'zh-CN',
+    title: '深入架构原理与实践',
+    description: '高可用服务体系原理与实践',
+    theme: defaultTheme({
+        navbar: [{
+                text: '首页',
+                link: '/'
+            },
+            {
+                text: 'GitHub仓库',
+                link: 'https://github.com/isno/theByteBook'
+            }
+        ],
+        sidebar: [{
+            title: '网络',
+            collapsable: false,
+            children: [
+                '/docs/chapter1/intro.md'
+            ]
+        }]
+    })
+});
