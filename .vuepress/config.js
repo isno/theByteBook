a82c733302234191e738685a5677bc9f6f148f83
isno <isno.cn@gmail.com>
2023-03-12 19:10:03 +0800

    更新内容

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,6 +4,9 @@ export default defineUserConfig({
     lang: 'zh-CN',
     title: '深入架构原理与实践',
     description: '高可用服务体系原理与实践',
+    head: [
+        ['link', { rel: "shortcut icon", href: "/assets/favicon.ico" }],
+    ],
     theme: defaultTheme({
         navbar: [{
                 text: '首页',
@@ -15,10 +18,13 @@ export default defineUserConfig({
             }
         ],
         sidebar: [{
-            title: '网络',
+            text: '网络原理与优化指南',
             collapsable: false,
             children: [
-                '/content/chapter1/intro.md'
+                '/content/chapter1/intro.md',
+                '/content/chapter1/wireless.md',
+                '/content/chapter1/latency.md',
+                '/content/chapter1/netfilter.md',
             ]
         }]
     })
