38cb324120b44dacdfe48ec90addc204119a8934
isno <isno.cn@gmail.com>
2023-04-10 22:45:12 +0800

    更新BGP内容

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,11 +1,19 @@
 import { defineUserConfig, defaultTheme } from 'vuepress';
 
+import { readingTimePlugin } from "vuepress-plugin-reading-time2";
+
+
 export default defineUserConfig({
     lang: 'zh-CN',
     title: '深入架构原理与实践',
     description: '高可用服务体系原理与实践',
     head: [
         ['link', { rel: "shortcut icon", href: "/assets/favicon.ico" }],
+    ],
+    plugins: [
+        readingTimePlugin({
+      // 你的选项
+        })
     ],
     theme: defaultTheme({
         navbar: [{
