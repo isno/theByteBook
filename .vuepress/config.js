96759ab10d63961553aaf0a06df74aea5522b473
isno <isno.cn@gmail.com>
2023-12-14 14:30:04 +0800

    增加分布式内容导图

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -213,6 +213,7 @@ export default defineUserConfig({
                 link: '/consensus/summary.md',
                 sidebarDepth: 2,
                 children: [
+                    '/consensus/consensus.md',
                     '/consensus/The-Byzantine-General-Problem.md',
                     {
                         text: "6.3 Paxos 算法",
